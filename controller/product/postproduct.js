ef74a9a9b38d4e31e1a5d6ddea2dc2bd4cfd28cb
santo matthew <matthewalesandro78@gmail.com>
2022-06-16 12:16:24 +0700

    minimalize bugs on post & put product

--- controller/product/postproduct.js
+++ controller/product/postproduct.js
@@ -1,26 +1,28 @@
-const { Products } = require("../../models")
+const { Products } = require("../../models");
 
 async function postProduct(req, res) {
-    const inputName = req.body.name
-    const inputPrice = req.body.price
-    const inputCategory = req.body.category
-    const inputDescription = req.body.inputDescription
-    const inputPhoto = req.body.inputPhoto
+  try {
+    const inputName = req.body.name;
+    const inputPrice = req.body.price;
+    const inputCategory = req.body.category;
+    const inputDescription = req.body.description;
+    const inputPhoto = req.body.photo;
 
-    const newProduct = await Products.create ({
-        name: inputName, 
-        price: inputPrice,
-        category_id: inputCategory,
-        description: inputDescription,
-        photo: inputPhoto
-    })
-    if(newProduct) {
-        res.status(201).send(`Product ${inputName} berhasil dibuat`)
+    const newProduct = await Products.create({
+      name: inputName,
+      price: inputPrice,
+      category_id: inputCategory,
+      description: inputDescription,
+      photo: inputPhoto,
+    });
+    if (newProduct) {
+      res.status(201).send(`Product ${inputName} berhasil dibuat`);
+    } else {
+      res.status(424).send(`Product tidak berhasil dibuat`);
     }
-    else{
-        res.status(424).send(`Product tidak berhasil dibuat`)
-    }
-
+  } catch (error) {
+    res.send(error);
+  }
 }
 
 module.exports = postProduct;
--- controller/product/putproduct.js
+++ controller/product/putproduct.js
@@ -1,20 +1,38 @@
-const { Products } = require("../../models")
+const { Products } = require("../../models");
 
-async function putProduct(req, res){
-    const inputName=req.body.name;
-    const inputPrice=req.body.price;
-    const inputDescription=req.body.description;
-    const inputPhoto=req.body.photo;
-    const inputCategory=req.body.category;
+async function putProduct(req, res) {
+  try {
+    const inputId = req.params.id;
+    const inputName = req.body.name;
+    const inputPrice = req.body.price;
+    const inputDescription = req.body.description;
+    const inputPhoto = req.body.photo;
+    const inputCategory = req.body.category;
 
-    await Products.update({
-        name: inputName, 
-        price: inputPrice,
-        category_id: inputCategory,
-        description: inputDescription,
-        photo: inputPhoto
-    },{where:{id:req.params.id}})
+    const product = await Products.findByPk(inputId);
 
-    res.status(204).send(`update product ${req.params.id} berhasil`)
+    if (product) {
+      const updateProduct = await Products.update(
+        {
+          name: inputName,
+          price: inputPrice,
+          category_id: inputCategory,
+          description: inputDescription,
+          photo: inputPhoto,
+        },
+        { where: { id: inputId } }
+      );
+
+      if (updateProduct) {
+        res.send(`update product ${req.params.id} berhasil`);
+      } else {
+        res.send("update product gagal");
+      }
+    } else {
+      res.send("Produk yang ingin di update tidak ada");
+    }
+  } catch (error) {
+    res.send(error);
+  }
 }
-module.exports=putProduct
+module.exports = putProduct;
