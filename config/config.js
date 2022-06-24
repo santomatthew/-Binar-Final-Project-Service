5cef0da8f9eb4c8a3c00806ae96347b16c5285a9
santo matthew <matthewalesandro78@gmail.com>
2022-06-24 12:41:09 +0700

    deployment settings more

--- config/config.js
+++ config/config.js
@@ -28,6 +28,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
+    use_env_variable: `DATABASE_URL`,
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
