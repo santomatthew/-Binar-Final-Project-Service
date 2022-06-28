3d521406659aa5312ea271a77685236a25800bff
santo matthew <matthewalesandro78@gmail.com>
2022-06-29 01:39:31 +0700

    add column on offers table

---
+++ migrations/20220628183245-offercolumn.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn("offers", "status", Sequelize.BOOLEAN);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
