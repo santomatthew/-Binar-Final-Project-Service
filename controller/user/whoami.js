e2e84dedbe6c1223ec364678262872102858d908
santo matthew <matthewalesandro78@gmail.com>
2022-07-20 21:09:24 +0700

    modify whoami to provide user photo

--- controller/user/whoami.js
+++ controller/user/whoami.js
@@ -1,4 +1,4 @@
-const { Users } = require("../../models");
+const { Users, Photos } = require("../../models");
 
 async function whoAmI(req, res) {
   try {
@@ -6,12 +6,14 @@ async function whoAmI(req, res) {
 
     let person = await Users.findByPk(userData.id);
 
+    let photo = await Photos.findOne(person.photo_id);
     res.status(200).json({
       user_data: {
         id: person.id,
         name: person.name,
         city: person.city,
         address: person.address,
+        photo: photo.name,
         phone_number: person.phone_number,
       },
     });
