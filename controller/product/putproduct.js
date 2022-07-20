597ed45a89948654fa495cd5aa69f7120f4b5f3a
santo matthew <matthewalesandro78@gmail.com>
2022-07-21 00:46:44 +0700

    modify putproduct to update photo

--- controller/product/putproduct.js
+++ controller/product/putproduct.js
@@ -1,4 +1,4 @@
-const { Products, Notifications } = require("../../models");
+const { Products, Notifications, Photos } = require("../../models");
 
 async function putProduct(req, res) {
   try {
@@ -6,6 +6,8 @@ async function putProduct(req, res) {
     const inputId = req.params.id;
 
     const product = await Products.findByPk(inputId);
+
+    const inputPhoto = req.body.photo;
 
     if (product) {
       if (product.user_id == userData.id) {
@@ -22,6 +24,40 @@ async function putProduct(req, res) {
         });
 
         if (updateProduct) {
+          if (inputPhoto) {
+            let indexOfPhotos = 0;
+            const productPhotos = await Photos.findAll({
+              where: { product_id: product.id },
+            });
+            for (let i in inputPhoto) {
+              const checkProductPhotos = await Photos.findAll({
+                where: { product_id: product.id },
+              });
+              if (checkProductPhotos.length < 4) {
+                await Photos.create({
+                  name: inputPhoto[i],
+                  product_id: product.id,
+                });
+              } else {
+                await Photos.update(
+                  {
+                    name: inputPhoto[i],
+                  },
+                  {
+                    where: {
+                      id: productPhotos[indexOfPhotos].id,
+                    },
+                  }
+                );
+                if (indexOfPhotos < 4) {
+                  indexOfPhotos++;
+                } else {
+                  indexOfPhotos = 0;
+                }
+              }
+            }
+          }
+
           await Notifications.create({
             user_id: userData.id,
             product_id: inputId,
